3216117f5f071b005a3548fc0c7b5f55f04f3edc
splemb <60625895+splemb@users.noreply.github.com>
2021-12-09 16:55:52 +0000

    update

--- Assets/TouchMovement.cs
+++ Assets/TouchMovement.cs
@@ -7,11 +7,13 @@ public class TouchMovement : MonoBehaviour
     Rigidbody rb;
     Vector3 touchPos;
     [SerializeField] Transform model;
+    Camera inputCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        inputCamera = Camera.main.GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
@@ -23,21 +25,28 @@ public class TouchMovement : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
+
+            rb.drag = 2f;
             Touch touch = Input.GetTouch(0);
-            touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.transform.position.y));
+
+            touchPos = new Vector3(touchPos.x, transform.position.y, touchPos.z);
             Debug.Log(touch.position + " || " + touchPos);
-            touchPos = new Vector3(touchPos.x, 0, touchPos.y);
 
             Vector3 playerPos = transform.position;
             Vector3 toPoint = (touchPos - playerPos);
 
-            
 
-            if (toPoint.magnitude > 0.1f)
+            if (toPoint.magnitude > 0.01f)
             {
-                rb.AddForce(toPoint.normalized * Time.deltaTime * 60f * 50f);
-                model.rotation = Quaternion.FromToRotation(Vector3.forward, toPoint);
+                rb.AddForce(toPoint.normalized * Mathf.Clamp(toPoint.magnitude, 0, 2f) * Time.deltaTime * 60f * 5f);
+                Vector3 lookDir = touchPos - transform.position;
+                lookDir.y = 0; // keep only the horizontal direction
+                model.rotation = Quaternion.LookRotation(lookDir);
+                //model.rotation = Quaternion.FromToRotation(Vector3.forward, toPoint);
             }
         }
+
+        else { rb.drag = 5f; }
     }
 }
